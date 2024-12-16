4cf10bfbda60888193235fd8ad19fe076da5b56a
LudlowCode <90901975+LudlowCode@users.noreply.github.com>
2024-12-16 12:38:36 +0000

    responds to get now

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const { MongoClient } = require("mongodb");
+
+const app = require("express")();
+
 // Replace the uri string with your connection string.
 const uri = "mongodb+srv://LudlowReader:I0op4qBHMCGz1bMt@cluster0.6m5on.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
 const client = new MongoClient(uri);
+const database = client.db('pokemon');
+const pokes = database.collection('pokes');
 
-async function run() {
-  try {
-    const database = client.db('pokemon');
-    const pokes = database.collection('pokes');
-    // Query for a pokemon number 1
-    const query = { id: '001' };
-    const poke = await pokes.findOne(query);
-    console.log(poke);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const PORT = 3000;
+
+
+// Define a simple route for HTTP GET requests
+app.get('/', async (req, res) => {
+    try {
+      const output = await pokes.find().toArray();
+      console.log(output);
+      res.send(output);
+  
+    } catch (err) {
+      res.status(500).json({ message: 'Error fetching data from MongoDB)', error: err });
+    }
+  });
+  app.listen(PORT, () => {
+    console.log(`Express server running at http://localhost:${PORT}/`);
+  });
