9df097093c7f5e734ad90640d766db163676bd8f
LudlowCode <90901975+LudlowCode@users.noreply.github.com>
2025-01-31 11:37:54 +0000

    try this

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const { MongoClient } = require("mongodb");
+const cors=require('cors');
+
 // Replace the uri string with your connection string.
 const uri = "mongodb+srv://LudlowReader:I0op4qBHMCGz1bMt@cluster0.6m5on.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(uri);
+// Initialize Express app
+const app = express();
+app.use(cors())
+const database = client.db('pokemon');
+const pokes = database.collection('pokes');
 
-async function run() {
+// Define a simple route for HTTP GET requests
+app.get('/', async (req, res) => {
   try {
-    const database = client.db('pokemon');
-    const pokes = database.collection('pokes');
-    // Query for a pokemon number 1
-    const query = { id: '001' };
-    const poke = await pokes.findOne(query);
-    console.log(poke);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    res.set('Access-Control-Allow-Origin', '*');
+    let results = await pokes.find({})
+    .limit(50)
+    .toArray();
+    res.send(results).status(200);
+
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching data from MongoDB', error: err });
   }
-}
-run().catch(console.dir);
+});
+
+
+// Create HTTP server and listen on port 3000
+http.createServer(app).listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+
+});
